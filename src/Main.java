21070d8f712528400d033506f7d6c3e66190b752
ALittleLazy <980393804@qq.com>
2017-12-10 21:43:20 +0800

    l

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        //fewifkweijfoiweeqeqwgrgghuiowfedfjiewfjiooiqwjeojiojfioewf
+        //ll
         System.out.println("Hello World!");
     }
 }
