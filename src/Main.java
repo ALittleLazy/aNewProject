d1f8caf7fbdd2614b0d74953f9cbbeab9871d2ff
ALittleLazy <980393804@qq.com>
2017-12-14 22:36:11 +0800

    l

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import com.jframedemo.main_jframe;
 public class Main {
 
     public static void main(String[] args) {
-        //
+
         //System.out.println("Hello World!");
         main_jframe i = new main_jframe();
     }
